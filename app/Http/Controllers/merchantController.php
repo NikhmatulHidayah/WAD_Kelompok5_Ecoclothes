6b9b05b6402f7a8a7ed75f4305e79f327136f495
Nopal <naufalakmalrabbani@gmail.com>
2024-12-29 13:32:18 +0700

    commit merchant

--- app/Http/Controllers/merchantController.php
+++ app/Http/Controllers/merchantController.php
@@ -28,7 +28,9 @@ class merchantController extends Controller
         return view("admin.login");
     }
     public function getMerchantDashboard(){
-        return view(view: "admin.merchant");
+        $merchants = DB::table('merchants')->get();
+        //dd($merchants);
+        return view('admin.merchant', ['merchants' => $merchants]);
     }
     public function getAddMerchant(){
         $id_user = Auth::user()->id_user;
@@ -58,6 +60,82 @@ class merchantController extends Controller
         ]);
 
         return redirect("/admin/merchant/setting");
+    }
+
+    public function getProductList($id_merchant)
+    {
+        $merchant = DB::table('merchants')
+            ->where('id_merchant', $id_merchant)
+            ->select('name_merchant')
+            ->first();
+
+        $products = DB::table('products')
+        ->where('id_merchant', $id_merchant)
+        ->get();
+    
+        return view('admin.productList', [
+            'id_merchant' => $id_merchant,
+            'name_merchant' => $merchant->name_merchant ?? 'Merchant not found',
+            'products' => $products
+        ]);
+    }
+
+    public function getAddProduct($id_merchant){
+        $merchant = DB::table('merchants')
+        ->where('id_merchant', $id_merchant)
+        ->select('name_merchant')
+        ->first();
+
+        return view('admin.addProduct', [
+            'id_merchant' => $id_merchant,
+            'name_merchant' => $merchant->name_merchant ?? 'Merchant not found'
+        ]);
+    }
+
+    public function postAddProduct(Request $request, $id_merchant){
+        $id_merchant = $request->input('id_merchant');
+        $name_product = $request->input('name_product');
+        $size = $request->input('size');
+        $weight = $request->input('weight');
+        $category = $request->input('category');
+        $picture_1 = $request->input('picture_1');
+        $picture_2 = $request->input('picture_2');
+        $picture_3 = $request->input('picture_3');
+        $picture_4 = $request->input('picture_4');
+        $picture_5 = $request->input('picture_5');
+
+        DB::table('products')->insert([
+            'id_product' => Str::uuid(),
+            'id_merchant' => $id_merchant,
+            'name_product' => $name_product,
+            'size' => $size,
+            'weight' => $weight,
+            'category' => $category,
+            'picture_1' => $picture_1,
+            'picture_2' => $picture_2,
+            'picture_3' => $picture_3,
+            'picture_4' => $picture_4,
+            'picture_5' => $picture_5,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        return redirect("/admin/merchant/product/all/$id_merchant");
+    }
+
+    public function getListarticle(){
+        $articles = DB::table(table: 'articles')->get();
+
+        return view('admin.article', [
+            'articles' => $articles,
+        ]);
+    }
+
+    public function getAddArticle(Request $request){
+        return view("admin.addArticle");
+    }
+    public function postAddArticle(Request $request){
+        
     }
 
 }
